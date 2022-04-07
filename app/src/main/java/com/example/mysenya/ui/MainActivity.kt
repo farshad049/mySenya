e01668a91eee1362e0d0dd29a9c559e985f8d0bf
admin <farshad049@gmail.com>
2022-04-07 15:58:00 +0430

    safe arg added

--- app/src/main/java/com/example/mysenya/ui/MainActivity.kt
+++ app/src/main/java/com/example/mysenya/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.mysenya
+package com.example.mysenya.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.mysenya.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/com/example/mysenya/ui/fragment/HomeFragment.kt
@@ -0,0 +1,6 @@
+package com.example.mysenya.ui.fragment
+
+import androidx.fragment.app.Fragment
+
+class HomeFragment:Fragment() {
+}
